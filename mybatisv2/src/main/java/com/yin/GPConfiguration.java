ab45bcfc32cb1d2ba2b113ce0e28037675441ab9
Eason Yin <easony@synnex.com>
2018-05-21 16:42:41 +0800

    mybatisv2

---
+++ mybatisv2/src/main/java/com/yin/GPConfiguration.java
@@ -0,0 +1,14 @@
+package com.yin;
+
+/**
+ * Created by easony on 05/21/18.
+ */
+public class GPConfiguration {
+
+    public MapperRegistor getMapperRegistor() {
+        return mapperRegistor;
+    }
+
+    private MapperRegistor mapperRegistor = new MapperRegister();
+
+}
---
+++ mybatisv2/src/main/java/com/yin/GPExecutor.java
@@ -0,0 +1,8 @@
+package com.yin;
+
+/**
+ * Created by easony on 05/21/18.
+ */
+public interface GPExecutor {
+    public <E> E query();
+}
